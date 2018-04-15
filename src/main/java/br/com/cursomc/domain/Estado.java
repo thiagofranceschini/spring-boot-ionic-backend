4ba706b052c2b48ea2df6e3da43b31d068a3a19c
Thiago Franceschini <thiagoconsultor1@gmail.com>
2018-04-15 18:25:12 -0300

    jsonIgnore replace jsonBackReference

--- src/main/java/br/com/cursomc/domain/Estado.java
+++ src/main/java/br/com/cursomc/domain/Estado.java
@@ -110,7 +110,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Estado implements Serializable {
@@ -121,7 +121,7 @@ public class Estado implements Serializable {
 	private Integer id;
 	private String nome;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="estado")
 	private List<Cidade> cidades = new ArrayList<>();
 	
