b46733ce50b00575dbba84de5b3656eafa09cdfd
Thiago Franceschini <thiagoconsultor1@gmail.com>
2018-04-15 22:03:13 -0300

    first Dto Created

--- src/main/java/br/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package br.com.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.cursomc.domain.Categoria;
+import br.com.cursomc.dto.CategoriaDTO;
 import br.com.cursomc.services.CategoriaService;
 
 @RestController
@@ -45,6 +48,14 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List <CategoriaDTO>> findAll() {
+		List<Categoria> list=service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDTO);
+		
 	}
 
 }
